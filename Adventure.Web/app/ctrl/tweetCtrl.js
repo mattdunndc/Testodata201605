3851a9e5afcf53aa04673851cae94f929ecbb805
MattDunnDC <dunn.matt@comcast.net>
2016-06-06 20:09:19 -0400

    added tweets untracked

---
+++ Adventure.Web/app/ctrl/tweetCtrl.js
@@ -0,0 +1,16 @@
+﻿var tweetCtrl = function($scope, notificationFactory, apiService)
+{
+        // Get Top 10 Employees
+    notificationFactory.info('Loading data, please wait...');
+    getTweets(); //init instead of ng-init in view
+    function getTweets () {
+        apiService.getTweets('mattdunndc')
+            .success(function (tweets) {
+                $scope.tweets = tweets;
+            })
+            .error(function (error) {
+                notificationFactory.error('Unable to load data '+error.message);
+            });
+    }
+}
+mod.controller('tweetCtrl', tweetCtrl);
